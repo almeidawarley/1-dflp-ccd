9b542253c84506916c0bd131bcc9ce0652cf0b29
Warley Almeida <almeida.warley@outlook.com>
2022-11-02 13:03:22 -0400

    Added changes to the graph script, and further modifications on the prototype

--- graphs.py
+++ graphs.py
@@ -10,20 +10,20 @@ def apply_absorption(demand, gamma, delta):
 
     return (1 - gamma) * demand - delta
 
-time_periods = [i for i in range(0, 10)]
+time_periods = [i for i in range(0, 6)]
 
 computed_demands = []
 predicted_demands = []
 
 # Replenishment
-alpha = 1
-beta  = 0
+alpha = 0
+beta  = 1
 
 # Absorption
 gamma = 0
-delta = 0
+delta = 2
 
-demand_ref = 1
+demand_ref = 6
 
 # ------------------------------------------------------------------------
 # Compute actual demands based on how the problem works
@@ -40,8 +40,13 @@ for t in time_periods:
     # demand_next = apply_replenishment(demand_previous, alpha, beta)
     demand_next = apply_replenishment(demand_previous, alpha, beta)
 
+    if t == 2:
+        d = 0
+    else:
+        d = delta
+
     # The absorption step happens if captured
-    demand_next = apply_absorption(demand_next, gamma, delta)
+    demand_next = apply_absorption(demand_next, gamma, d)
     # dn = absorption(dp, gamma * 10* (len(ts) - t), delta)
 
     demand_previous = demand_next
@@ -63,11 +68,22 @@ for t in time_periods:
         demand_lin = (beta - delta) * (t) + demand_previous
         demand_exp = ((1 + alpha) * (1 - gamma))**(t) * demand_previous
 
-        demand_next = demand_exp
+        demand_next = demand_lin
         predicted_demands.append(demand_next)
 
 
 plt.plot(time_periods, computed_demands)
 # plot(time_periods, computed_demands,'o')
-plt.plot(time_periods, predicted_demands, 'x')
-plt.show()
+# plt.plot(time_periods, predicted_demands, 'x')
+# plt.show()
+
+for t in time_periods:
+
+    print('\draw ({},0) node[anchor=north east] {}$t_{}${};'.format(t + 1.25, '{', t, '}'))
+    print('\draw (-0.25,{}) node[anchor=north east] {}$d^{}t_{}{}_{}${};'.format(computed_demands[t] + 0.25, '{', '{', t, '}', '{j}','}'))
+    print('\draw (-0.15,{}) -- (0.15, {});'.format(computed_demands[t], computed_demands[t]))
+
+    xl = t + 1 - 0.1
+    xr = t + 1 + 0.1
+
+    print('\draw[fill=gray!50] ({},0) -- ({},0) -- ({},{}) -- ({},{}) -- ({},0);'.format(xl, xr, xr, computed_demands[t], xl, computed_demands[t], xl))
