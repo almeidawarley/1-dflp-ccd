9df3aee6d66f2c1fd0edfa6603ab4330745b7183
Warley Almeida <almeida.warley@outlook.com>
2022-11-12 17:00:26 -0500

    Fixed missing line break in scripts, removed export file

--- exporting.sh
+++ exporting.sh
@@ -1,3 +1,3 @@
 scp instances/* walm@beluga.computecanada.ca:~/shortcut/instances/
 scp scripts/* walm@beluga.computecanada.ca:~/shortcut/scripts/
-scp commands.sh walm@beluga.computecanada.ca:~/shortcut/outputs/
+scp commands*.sh walm@beluga.computecanada.ca:~/shortcut/outputs/
--- generator.py
+++ generator.py
@@ -28,16 +28,16 @@ with open('commands.sh','w') as commands:
 
                                 if counter >= 0:
 
-                                    keyword = '{}-{}-{}-{}-{}-{}-{}-{}-{}'.format('A', s, i, i, t, z, r, a, e)
+                                    keyword = '{}-{}-{}-{}-{}-{}-{}-{}-{}'.format('A', s, i, i, t, r, z, a, e)
                                     instance = {}
                                     instance['S'] = s
                                     instance['I'] = i
                                     instance['J'] = i
                                     instance['T'] = t
-                                    instance['zeta'] = z
-                                    instance['eta'] = e
                                     instance['replenishment'] = r
+                                    instance['zeta'] = z
                                     instance['absorption'] = a
+                                    instance['eta'] = e
 
                                     with open('{}/{}.json'.format('instances', keyword), 'w') as output:
                                         js.dump(instance, output)
@@ -56,7 +56,7 @@ with open('commands.sh','w') as commands:
                                         output.write('#SBATCH --mail-type=FAIL\n')
 
                                         output.write('cd ~/shortcut/\n')
-                                        output.write('wandb offline')
+                                        output.write('wandb offline\n')
                                         output.write('python main.py -p {} {}\n'.format(experiment, keyword))
 
                                     commands.write('dos2unix ../scripts/{}.sh\n'.format(keyword))
