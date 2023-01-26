48fb0e52520d9bd2d2f29bedc7cc152b3747a5ea
Warley Almeida <almeida.warley@outlook.com>
2023-01-26 13:23:59 -0500

    Added estimation model for the simples case

---
+++ estimation.py
@@ -0,0 +1,196 @@
+import gurobipy as gp
+import variables as vb
+import formulation as fm
+import instance as ic
+
+def previous(period):
+
+    return str(int(period) - 1)
+
+def create_vrb(instance, mip):
+    # Create beta_{j} variables
+
+    lowers = [0 for _ in instance.customers]
+    uppers = [gp.GRB.INFINITY for _ in instance.customers]
+    coefs = [0.  for _ in instance.customers]
+    types = ['C' for _ in instance.customers]
+    names = [
+        'b~{}'.format(customer)
+        for customer in instance.customers
+    ]
+
+    return mip.addVars(instance.customers, lb = lowers, ub = uppers, obj = coefs, vtype = types, name = names)
+
+# ---------------------------------------------------------------------------
+
+def create_vrw(instance, mip):
+    # Create w^{t}_{j} variables
+
+    lowers = [0 for _ in instance.periods for _ in instance.customers]
+    uppers = [gp.GRB.INFINITY for _ in instance.periods for _ in instance.customers]
+    coefs = [0.  for _ in instance.periods for _ in instance.customers]
+    types = ['C' for _ in instance.periods for _ in instance.customers]
+    names = [
+        'w~{}_{}'.format(period, customer)
+        for period in instance.periods
+        for customer in instance.customers
+    ]
+
+    return mip.addVars(instance.periods, instance.customers, lb = lowers, ub = uppers, obj = coefs, vtype = types, name = names)
+
+# ---------------------------------------------------------------------------
+
+def create_vrd(instance, mip):
+    # Create delta_{j} variables
+
+    lowers = [0 for _ in instance.customers]
+    uppers = [gp.GRB.INFINITY for _ in instance.customers]
+    coefs = [0.  for _ in instance.customers]
+    types = ['C' for _ in instance.customers]
+    names = [
+        'd~{}'.format(customer)
+        for customer in instance.customers
+    ]
+
+    return mip.addVars(instance.customers, lb = lowers, ub = uppers, obj = coefs, vtype = types, name = names)
+
+# ---------------------------------------------------------------------------
+
+def create_vrd0(instance, mip):
+    # Create D^0_{j} variables
+
+    lowers = [0 for _ in instance.customers]
+    uppers = [gp.GRB.INFINITY for _ in instance.customers]
+    coefs = [0.  for _ in instance.customers]
+    types = ['C' for _ in instance.customers]
+    names = [
+        'd0~{}'.format(customer)
+        for customer in instance.customers
+    ]
+
+    return mip.addVars(instance.customers, lb = lowers, ub = uppers, obj = coefs, vtype = types, name = names)
+
+# ---------------------------------------------------------------------------
+
+def create_c1(instance, mip, variable):
+    # Create constraint 1
+
+    mip.addConstrs((variable['d3']['0', customer] - variable['d0'][customer] == 0 for customer in instance.customers), name = 'c1')
+    # mip.addConstrs((variable['d3']['0', customer] == 0 for customer in instance.customers), name = 'c1')
+
+# ---------------------------------------------------------------------------
+
+def create_c2(instance, mip, variable):
+    # Create constraint 2
+
+    mip.addConstrs((variable['d3'][period, customer] == variable['d3'][previous(period), customer] + variable['b'][customer] - variable['w'][period, customer] for period in instance.periods for customer in instance.customers), name = 'c2')
+
+# ---------------------------------------------------------------------------
+
+def create_c3A(instance, mip, variable, indicators):
+    # Create constraint 3, part A
+
+    mip.addConstrs((variable['w'][period, customer] <= indicators[period][customer] * (variable['d'][customer]) for period in instance.periods for customer in instance.customers), name = 'c3A')
+
+def create_c3B(instance, mip, variable, indicators):
+    # Create constraint 3, part B
+
+    mip.addConstrs((variable['w'][period, customer] <= indicators[period][customer] * (variable['d3'][previous(period), customer] + variable['b'][customer]) for period in instance.periods for customer in instance.customers), name = 'c3B')
+
+def create_c3C(instance, mip, variable, indicators):
+    # Create constraint 3, part C
+
+    mip.addConstrs((variable['w'][period, customer] >= indicators[period][customer] * (variable['d'][customer] - instance.bigM[customer] * (1 - variable['u'][period, customer])) for period in instance.periods for customer in instance.customers), name = 'c3C')
+
+def create_c3D(instance, mip, variable, indicators):
+    # Create constraint 3, part D
+
+    mip.addConstrs((variable['w'][period, customer] >= indicators[period][customer] * (variable['d3'][previous(period), customer] + variable['b'][customer] - instance.bigM[customer] * variable['u'][period, customer]) for period in instance.periods for customer in instance.customers), name = 'c3D')
+
+# ---------------------------------------------------------------------------
+
+def build_estimation(instance, training):
+    # Build the estimation model
+
+    mip = gp.Model('EST-1-DFLP-DRA')
+
+    # Create decision variables
+    variable = {
+        'd3': vb.create_vrd3(instance, mip),
+        'u': vb.create_vru(instance, mip),
+        'w': create_vrw(instance, mip),
+        'b': create_vrb(instance, mip),
+        'd': create_vrd(instance, mip),
+        'd0': create_vrd0(instance, mip)
+    }
+
+    # Minimize error
+    mip.setAttr('ModelSense', +1)
+
+    # Turn off GUROBI logs
+    # mip.setParam('OutputFlag', 0)
+    # mip.setParam('NumericFocus', 3)
+    mip.setParam('Threads', 1)
+    mip.setParam('TimeLimit', 10 * 60 * 60)
+
+    # Set squared error as objective
+    mip.setObjective(sum([(variable['w'][period, customer] - training['w'][period][customer]) ** 2 for period in instance.periods for customer in instance.customers]))
+
+    # Build indicators for constraints
+    indicators = {}
+    for period in instance.periods:
+        indicators[period] = {}
+        for customer in instance.customers:
+            # In short, 1 if customer is captured during a period
+            indicators[period][customer] = sum([training['y'][period][location] * instance.catalogs[location][customer] for location in instance.locations])
+
+    # Create main constraints
+    create_c1(instance, mip, variable)
+    create_c2(instance, mip, variable)
+    create_c3A(instance, mip, variable, indicators)
+    create_c3B(instance, mip, variable, indicators)
+    create_c3C(instance, mip, variable, indicators)
+    create_c3D(instance, mip, variable, indicators)
+
+    return mip, variable
+
+# ---------------------------------------------------------------------------
+
+# Read instance from file
+instance = ic.instance('D-toy')
+
+# Build 1-DFLP-RA model
+mip, variable = fm.build_fancy(instance)
+# Find optimal solution
+mip.optimize()
+
+# Prepare training data
+training = {}
+training['w'] = {}
+training['y'] = {}
+
+for period in instance.periods:
+    training['w'][period] = {}
+    training['y'][period] = {}
+    for customer in instance.customers:
+        training['w'][period][customer] = sum([variable['w'][period, location, customer].x for location in instance.locations])
+        if training['w'][period][customer] > 0.:
+            print('| captured {} units from customer {} in period {}'.format(training['w'][period][customer], customer, period))
+    for location in instance.locations:
+        training['y'][period][location] = variable['y'][period, location].x
+        if training['y'][period][location] > 0.:
+            print('| visited location {} in period {}'.format(location, period))
+
+# Build EST-1-DFL-RA model
+est, variable = build_estimation(instance, training)
+# Find optimal parameters
+est.optimize()
+
+# est.write('est.lp')
+
+for customer in instance.customers:
+    print('For customer {}...'.format(customer))
+
+    print('| D^0: {}'.format(variable['d0'][customer].x))
+    print('| Beta: {}'.format(variable['b'][customer].x))
+    print('| Delta: {}'.format(variable['d'][customer].x))
