bc8f3c82e73175d84ea60d1304239a97dfae88bb
Warley Almeida <almeida.warley@outlook.com>
2023-02-16 13:34:33 -0500

    Changed a bit constraints to force some sampling

--- estimation.py
+++ estimation.py
@@ -184,17 +184,13 @@ instance.print_instance()
 
 # Build 1-DFLP-RA model
 mip, variable = fm.build_fancy(instance)
-'''
 try:
-    mip.addConstr(variable['y']['1', '1'] == 1)
-    mip.addConstr(variable['y']['2', '2'] == 1)
-    mip.addConstr(variable['y']['3', '3'] == 1)
-    mip.addConstr(variable['y']['4', '4'] == 1)
-    mip.addConstr(variable['y']['5', '5'] == 1)
+    for period in instance.periods:
+        mip.addConstr(variable['y'][period, period] == 1)
 except Exception as e:
     print('Not able to add one of the constraints')
     print('Error message: {}'.format(e))
-'''
+
 # Find optimal solution
 mip.optimize()
 org_solution = fm.format_solution(instance, mip, variable)
