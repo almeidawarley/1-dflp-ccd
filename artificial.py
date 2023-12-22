090d8b5f5730370c928c56f470bb58bdbc0de6c9
Warley Almeida <almeida.warley@outlook.com>
2023-12-22 16:37:51 -0500

    Finished writing code for new instances

--- artificial.py
+++ artificial.py
@@ -9,7 +9,7 @@ class artificial(ic.instance):
         super().__init__(keyword, project)
 
     def create_instance(self, folder = 'instances/artificial'):
-        # Create rnd-based instances
+        # Create artificial instances
 
         try:
             # Read specifications from file
@@ -20,26 +20,24 @@ class artificial(ic.instance):
 
         np.random.seed(self.parameters['seed'])
 
-        # Set instance information
+        # Create instance sets
         number_locations = int(self.parameters['locations'])
-        number_customers = int(self.parameters['customers'])
-        number_periods = int(self.parameters['periods'])
-
         self.locations = [str(i + 1) for i in range(number_locations)]
+        number_customers = int(self.parameters['customers'])
         self.customers = [str(i + 1) for i in range(number_customers)]
-        self.periods = [str(i + 1) for i in range(number_periods)]
+        number_periods = int(self.parameters['periods'])
+        self.periods = [int(i + 1) for i in range(number_periods)]
 
+        # Create random preferences
         if self.parameters['patronizing'] == 'small':
-            patronizing = 1 / 2.01
+            patronizing = 0.5 # 1 / 2.01
         elif self.parameters['patronizing'] == 'medium':
             patronizing = 1
         elif self.parameters['patronizing'] == 'large':
-            patronizing = 2.01
+            patronizing = 2 # 2.01
         else:
             exit('Wrong value for patronizing parameter')
-
-        consideration_size = int(np.ceil((patronizing *number_locations)/number_periods))
-
+        consideration_size = int(np.ceil((patronizing * number_locations) / number_periods))
         consideration_sets = {}
         for customer in self.customers:
             consideration_sets[customer] = np.random.choice(self.locations, consideration_size)
@@ -63,48 +61,40 @@ class artificial(ic.instance):
                     coefficient = 1. / popularity
                 else:
                     exit('Wrong value for rewards parameter')
-                self.rewards[period][location] = np.ceil(coefficient * len(self.locations))
+                self.rewards[period][location] = np.ceil(coefficient * len(self.customers))
 
-        # Create parameters
-        self.alphas = {}
-        self.betas = {}
-        self.starts = {}
-        self.intensities = {}
+        stored_f = {}
+        def bass(t, p = 0.04, q = 0.4, m = 10):
+            return (p * m + (q - p) * sum(v for x, v in stored_f.items() if x <= t) - (q/m) * (sum(v for x, v in stored_f.items() if x <= t)) ** 2)
 
+        for period in self.periods:
+            stored_f[period] = bass(period)
+
+        # Create amplitude
+        self.amplitude = {}
         for customer in self.customers:
-            self.starts[customer] = 1 if self.parameters['character'] == 'homogeneous' else np.random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            self.intensities[customer] = 1 if self.parameters['character'] == 'homogeneous' else np.random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            if self.parameters['replenishment'] == 'none':
-                self.alphas[customer] = 0
-                self.betas[customer] = 0
-            elif self.parameters['replenishment'] == 'absolute':
-                self.alphas[customer] = 0
-                self.betas[customer] = self.intensities[customer]
-            elif self.parameters['replenishment'] == 'relative':
-                self.alphas[customer] = round(((number_periods * self.intensities[customer] + self.starts[customer])/self.starts[customer])**(1. / number_periods) - 1, 2)
-                self.betas[customer] = 0
-            elif self.parameters['replenishment'] == 'mixed':
-                def evaluate_combination(alpha, beta):
-                    result = self.starts[customer]
-                    for _ in self.periods:
-                        result += alpha * result + beta
-                    return abs(2 * (self.starts[customer] + self.intensities[customer] * len(self.periods)) - result)
-                def minimize_combination():
-                    best_alpha = .0
-                    best_beta = .0
-                    best_value = 10**4
-                    for alpha in range(1, 101):
-                        local_alpha = float(alpha/100)
-                        for beta in range(1, 11):
-                            local_beta = float(beta)
-                            value = evaluate_combination(local_alpha, local_beta)
-                            if value < best_value:
-                                best_value = value
-                                best_alpha = local_alpha
-                                best_beta = local_beta
-                    return best_alpha, best_beta
-                alpha, beta = minimize_combination()
-                self.alphas[customer] = alpha
-                self.betas[customer] = beta
+            if self.parameters['behaviour'] == 'heterogeneous':
+                self.amplitude[customer] = np.random.choice([0, 5, 10, 15, 20])
+            elif self.parameters['behaviour'] == 'homogeneous':
+                self.amplitude[customer] = 10
             else:
-                exit('Wrong value for replenishment parameter')
+                exit('Wrong value for behaviour parameter')
+
+        # Create spawning
+        self.spawning = {}
+        for period in self.periods:
+            self.spawning[period] = {}
+            for customer in self.customers:
+                if self.parameters['demand'] == 'constant':
+                    self.spawning[period][customer] = self.amplitude[customer]
+                elif self.parameters['demand'] == 'seasonal':
+                    self.spawning[period][customer] = (self.amplitude[customer] / 2) * np.cos(period) + (self.amplitude[customer] / 2)
+                elif self.parameters['demand'] == 'increasing':
+                    self.spawning[period][customer] = (period / number_periods) * self.amplitude[customer]
+                elif self.parameters['demand'] == 'decreasing':
+                    self.spawning[period][customer] = self.amplitude[customer] - ((period - 1)/ number_periods) * self.amplitude[customer]
+                elif self.parameters['demand'] == 'bass':
+                    self.spawning[period][customer] = stored_f[period] * self.amplitude[customer]
+                else:
+                    exit('Wrong value for demand behaviour')
+                self.spawning[period][customer] = np.ceil(self.spawning[period][customer])
