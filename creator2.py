272c15ffd6d336c9b7c590e55ab0c650780b5f85
Warley Almeida <almeida.warley@outlook.com>
2024-02-03 16:00:14 +0000

    Modified scripts to account for a method number

--- creator2.py
+++ creator2.py
@@ -10,6 +10,8 @@ features = {
     'demands': ['constant', 'seasonal']
 }
 
+methods = [1,2,3,4]
+
 project = 'p1slovakia'
 
 script = sys.argv[1]
@@ -26,41 +28,42 @@ for map in features['maps']:
         for distance in features['distances']:
             for rewards in features['rewards']:
                 for demands in features['demands']:
+                    for method in methods:
 
-                    parameters['seed'] = 0
-                    parameters['map'] = map
-                    parameters['periods'] = periods
-                    parameters['distance'] = distance
-                    parameters['rewards'] = rewards
-                    parameters['demands'] = demands
+                        parameters['seed'] = 0
+                        parameters['map'] = map
+                        parameters['periods'] = periods
+                        parameters['distance'] = distance
+                        parameters['rewards'] = rewards
+                        parameters['demands'] = demands
 
-                    keyword = '{}_{}'.format('slv', '-'.join([str(value) for value in parameters.values()]))
+                        keyword = '{}_{}'.format('slv', '-'.join([str(value) for value in parameters.values()]))
 
-                    with open('{}/{}.json'.format('instances/slovakia', keyword), 'w') as output:
-                        js.dump(parameters, output)
+                        with open('{}/{}.json'.format('instances/slovakia', keyword), 'w') as output:
+                            js.dump(parameters, output)
 
-                    # _ = cm.load_instance(keyword, 'validation')
+                        # _ = cm.load_instance(keyword, 'validation')
 
-                    with open('{}/{}_{}.sh'.format('scripts', script, keyword), 'w') as output:
+                        with open('{}/{}_{}_{}.sh'.format('scripts', script, method, keyword), 'w') as output:
 
-                        output.write('#!/bin/bash\n')
+                            output.write('#!/bin/bash\n')
 
-                        output.write('#SBATCH --time={}:00:00\n'.format(hours))
-                        output.write('#SBATCH --job-name={}_{}.job\n'.format(script, keyword))
-                        output.write('#SBATCH --output={}_{}.out\n'.format(script, keyword))
-                        output.write('#SBATCH --account=def-mxm\n')
-                        output.write('#SBATCH --mem=30GB\n')
-                        output.write('#SBATCH --cpus-per-task=1\n')
-                        output.write('#SBATCH --mail-user=<almeida.warley@outlook.com>\n')
-                        output.write('#SBATCH --mail-type=FAIL\n')
+                            output.write('#SBATCH --time={}:00:00\n'.format(hours))
+                            output.write('#SBATCH --job-name={}_{}_{}.job\n'.format(script, method, keyword))
+                            output.write('#SBATCH --output={}_{}_{}.out\n'.format(script, method, keyword))
+                            output.write('#SBATCH --account=def-mxm\n')
+                            output.write('#SBATCH --mem=30GB\n')
+                            output.write('#SBATCH --cpus-per-task=1\n')
+                            output.write('#SBATCH --mail-user=<almeida.warley@outlook.com>\n')
+                            output.write('#SBATCH --mail-type=FAIL\n')
 
-                        output.write('cd ~/shortcut/\n')
-                        output.write('python run-{}.py -p {} {}\n'.format(script, project, keyword))
+                            output.write('cd ~/shortcut/\n')
+                            output.write('python run-{}.py -p {} -m {} {}\n'.format(script, project, method, keyword))
 
-                    commands.write('dos2unix ../scripts/{}_{}.sh\n'.format(script, keyword))
-                    commands.write('sbatch ../scripts/{}_{}.sh\n'.format(script, keyword))
+                        commands.write('dos2unix ../scripts/{}_{}_{}.sh\n'.format(script, method, keyword))
+                        commands.write('sbatch ../scripts/{}_{}_{}.sh\n'.format(script, method,keyword))
 
-                    print(keyword)
-                    counter += 1
+                        print(keyword)
+                        counter += 1
 
 print('This script run wrote {} scripts in total!'.format(counter))
--- run-benders.py
+++ run-benders.py
@@ -9,6 +9,7 @@ def main():
     parser = ap.ArgumentParser(description = 'Run relevant formulations for some DSFLP-C instance')
     parser.add_argument('keyword', type = str, help = 'Instance keyword following established patterns')
     parser.add_argument('-p', '--project', default = 'dsflp-c', type = str, help = 'Instance project name')
+    parser.add_argument('-m', '--methods', default =  0, type = str, help = 'Method identification number')
     args = parser.parse_args()
 
     cm.mark_section('Generating instance parameters')
@@ -29,30 +30,35 @@ def main():
 
     cm.mark_section('Solving with branch-and-Benders')
 
-    cm.mark_section('Analytical subproblems')
-    benders1 = bd.benders(instance, 'analytical')
-    metadata = benders1.solve_bbc('bba')
-    record = rc.update_record(record, metadata)
+    if args.methods == '1' or args.methods == '0':
+        cm.mark_section('Analytical subproblems')
+        benders1 = bd.benders(instance, 'analytical')
+        metadata = benders1.solve_bbc('bba')
+        record = rc.update_record(record, metadata)
 
-    cm.mark_section('Duality subproblems')
-    benders2 = bd.benders(instance, 'duality')
-    metadata = benders2.solve_bbc('bbd')
-    record = rc.update_record(record, metadata)
+    if args.methods == '2' or args.methods == '0':
+        cm.mark_section('Duality subproblems')
+        benders2 = bd.benders(instance, 'duality')
+        metadata = benders2.solve_bbc('bbd')
+        record = rc.update_record(record, metadata)
 
     cm.mark_section('Solving with standard Benders')
 
-    cm.mark_section('Analytical subproblems')
-    benders3 = bd.benders(instance, 'analytical')
-    metadata = benders3.solve_std('bsa')
-    record = rc.update_record(record, metadata)
+    if args.methods == '3' or args.methods == '0':
+        cm.mark_section('Analytical subproblems')
+        benders3 = bd.benders(instance, 'analytical')
+        metadata = benders3.solve_std('bsa')
+        record = rc.update_record(record, metadata)
 
-    cm.mark_section('Duality subproblems')
-    benders4 = bd.benders(instance, 'duality')
-    metadata = benders4.solve_std('bsd')
-    record = rc.update_record(record, metadata)
+    if args.methods == '4' or args.methods == '0':
+        cm.mark_section('Duality subproblems')
+        benders4 = bd.benders(instance, 'duality')
+        metadata = benders4.solve_std('bsd')
+        record = rc.update_record(record, metadata)
 
-    for approach in ['bba', 'bbd', 'bsa', 'bsd']:
-        print('> {} approach: {} <{}> [{}]'.format(approach.upper(), record['{}_objective'.format(approach)], record['{}_runtime'.format(approach)], record['{}_solution'.format(approach)]))
+    if args.methods == '0':
+        for approach in ['bba', 'bbd', 'bsa', 'bsd']:
+            print('> {} approach: {} <{}> [{}]'.format(approach.upper(), record['{}_objective'.format(approach)], record['{}_runtime'.format(approach)], record['{}_solution'.format(approach)]))
 
 if __name__ == '__main__':
 
