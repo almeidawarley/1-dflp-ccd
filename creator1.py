559315f99506630829607ddef91532d495ed5468
Warley Almeida <almeida.warley@outlook.com>
2024-11-05 18:36:28 -0500

    Pushed new creator script for pushing Gurobis limits

--- creator1.py
+++ creator1.py
@@ -4,12 +4,13 @@ import sys
 
 features = {
     'seed': [i for i in range(0, 5)],
-    'points': [50, 100],
+    'locations': [50, 100],
+    'customers': [1, 2, 3, 4, 5],
     'periods': [10],
-    'facilities': [1, 2, 3, 4],
-    'penalties': [0, 1, 2, 3],
-    'preferences': ['small', 'large'],
-    'rewards': ['identical', 'inversely'],
+    'facilities': [1], # [1, 2, 3, 4],
+    'penalties': [3], # [0, 1, 2, 3],
+    'preferences': ['large'], # ['small', 'large'],
+    'rewards': ['inversely'], # ['identical', 'inversely'],
     'demands': ['constant'], # ['constant', 'seasonal', 'increasing', 'decreasing'],
     'characters': ['heterogeneous'] # ['homogeneous', 'heterogeneous']
 }
@@ -24,53 +25,54 @@ commands = open('commands_{}.sh'.format(script), 'w')
 counter = 0
 
 for seed in features['seed']:
-    for points in features['points']:
-        for periods in features['periods']:
-            for facilities in features['facilities']:
-                for penalties in features['penalties']:
-                    for preferences in features['preferences']:
-                        for rewards in features['rewards']:
-                            for demands in features['demands']:
-                                for character in features['characters']:
+    for locations in features['locations']:
+        for customers in features['customers']:
+            for periods in features['periods']:
+                for facilities in features['facilities']:
+                    for penalties in features['penalties']:
+                        for preferences in features['preferences']:
+                            for rewards in features['rewards']:
+                                for demands in features['demands']:
+                                    for character in features['characters']:
 
-                                    parameters['seed'] = seed
-                                    parameters['locations'] = points
-                                    parameters['customers'] = points
-                                    parameters['periods'] = periods
-                                    parameters['facilities'] = facilities
-                                    parameters['penalties'] = penalties
-                                    parameters['preferences'] = preferences
-                                    parameters['rewards'] = rewards
-                                    parameters['demands'] = demands
-                                    parameters['characters'] = character
+                                        parameters['seed'] = seed
+                                        parameters['locations'] = locations
+                                        parameters['customers'] = customers
+                                        parameters['periods'] = periods
+                                        parameters['facilities'] = facilities
+                                        parameters['penalties'] = penalties
+                                        parameters['preferences'] = preferences
+                                        parameters['rewards'] = rewards
+                                        parameters['demands'] = demands
+                                        parameters['characters'] = character
 
-                                    keyword = '{}_{}'.format('art', '-'.join([str(value) for value in parameters.values()]))
+                                        keyword = '{}_{}'.format('bmk', '-'.join([str(value) for value in parameters.values()]))
 
-                                    with open('{}/{}.json'.format('instances/artificial', keyword), 'w') as output:
-                                        js.dump(parameters, output)
+                                        with open('{}/{}.json'.format('instances/benchmark', keyword), 'w') as output:
+                                            js.dump(parameters, output)
 
-                                    # _ = cm.load_instance(keyword, 'validation')
+                                        # _ = cm.load_instance(keyword, 'validation')
 
-                                    with open('{}/{}_{}.sh'.format('scripts', script, keyword), 'w') as output:
+                                        with open('{}/{}_{}.sh'.format('scripts', script, keyword), 'w') as output:
 
-                                        output.write('#!/bin/bash\n')
+                                            output.write('#!/bin/bash\n')
 
-                                        output.write('#SBATCH --time={}:00:00\n'.format(hours))
-                                        output.write('#SBATCH --job-name={}_{}.job\n'.format(script, keyword))
-                                        output.write('#SBATCH --output={}_{}.out\n'.format(script, keyword))
-                                        output.write('#SBATCH --account=def-mxm\n')
-                                        output.write('#SBATCH --mem=30GB\n')
-                                        output.write('#SBATCH --cpus-per-task=1\n')
-                                        output.write('#SBATCH --mail-user=<almeida.warley@outlook.com>\n')
-                                        output.write('#SBATCH --mail-type=FAIL\n')
+                                            output.write('#SBATCH --time={}:00:00\n'.format(hours))
+                                            output.write('#SBATCH --job-name={}_{}.job\n'.format(script, keyword))
+                                            output.write('#SBATCH --output={}_{}.out\n'.format(script, keyword))
+                                            output.write('#SBATCH --account=def-mxm\n')
+                                            output.write('#SBATCH --mem=30GB\n')
+                                            output.write('#SBATCH --cpus-per-task=1\n')
+                                            output.write('#SBATCH --mail-user=<almeida.warley@outlook.com>\n')
+                                            output.write('#SBATCH --mail-type=FAIL\n')
 
-                                        output.write('cd /home/walm/projects/def-mxm/walm/1-dflp-ra/\n')
-                                        output.write('python run-{}.py {}\n'.format(script, keyword))
+                                            output.write('cd /home/walm/projects/def-mxm/walm/1-dflp-ra/\n')
+                                            output.write('python run-{}.py {}\n'.format(script, keyword))
 
-                                    commands.write('dos2unix ../scripts/{}_{}.sh\n'.format(script, keyword))
-                                    commands.write('sbatch ../scripts/{}_{}.sh\n'.format(script, keyword))
+                                        commands.write('dos2unix ../scripts/{}_{}.sh\n'.format(script, keyword))
+                                        commands.write('sbatch ../scripts/{}_{}.sh\n'.format(script, keyword))
 
-                                    print(keyword)
-                                    counter += 1
+                                        print(keyword)
+                                        counter += 1
 
 print('This script run wrote {} scripts in total!'.format(counter))
