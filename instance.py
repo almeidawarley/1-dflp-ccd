6a93e10386268726ef15ffcc11ff034a947edabf
Warley Almeida <almeida.warley@outlook.com>
2024-11-05 19:01:13 -0500

    Fixed a formatting issue2

--- instance.py
+++ instance.py
@@ -59,7 +59,7 @@ class instance:
 
         # Print instance summary
         print('Keyword: {}'.format(self.keyword))
-        print('Facilities: {}'.format(list(self.facilities.values())))
+        print('Facilities: {}'.format([number for number in self.facilities.values()]))
 
         # Print customer information
         print('Customers: {}'.format(self.customers))
